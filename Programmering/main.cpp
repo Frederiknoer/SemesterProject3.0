e181f1f539b1a34754f317cd99fad1e25d57618e
Frederiknoer <Frederikk@live.dk>
2016-10-08 14:07:49 +0200

    små ændringer

--- Programmering/main.cpp
+++ Programmering/main.cpp
@@ -3,38 +3,47 @@
 #include "Sound/sound.h"
 #include "TextHandler/TextHandler.h"
 #include "Frame/Frame/Frame.h"
-
 using namespace std;
 
 int main()
 {
     //Oprettelse af konstanter
     string inputText;
-    getline(cin, inputText);
     vector<int> HexBuffer;
     vector<int> FrameBuffer;
     TextHandler dataIn;
     HexBuffer.clear();
 
-    //Ligger teksten ind i HexBuffer som decimaler fra 0-15
-    for(int i = 0; i < (inputText.length() * 2); i++)
-    {
-        HexBuffer.push_back(dataIn.InputText(inputText)[i]);
+    while(true) {
+        //Læser input
+        getline(cin, inputText);
+
+        if (inputText == "stop")
+            break;
+
+        //Ligger teksten ind i HexBuffer som decimaler fra 0-15
+        vector<int> HexIntVec = dataIn.InputText(inputText);
+        for (int i = 0; i < (inputText.length() * 2); i++) {
+            HexBuffer.push_back(HexIntVec[i]);
+        }
+
+        //Opretter frame objekt
+        Frame framing(HexBuffer);
+        framing.makeFrame();
+
+        //Ligger hexbuffer ind i framebuffer med frame rundt om
+        for (int i = 0; i < framing.getLength(); i++) {
+            FrameBuffer.push_back(framing.getFrame().getElement(i));
+        }
+
+        //Afspiller lyd
+        Sound SoundOne;
+        SoundOne.playSound(FrameBuffer);
+
+        //Tømmer bufferne
+        HexBuffer.clear();
+        HexIntVec.clear();
+        FrameBuffer.clear();
     }
-
-    //Opretter frame objekt
-    Frame framing(HexBuffer);
-    framing.makeFrame();
-
-    //Ligger hexbuffer ind i framebuffer med frame rundt om
-    for (int i = 0; i < framing.getLength(); i++)
-    {
-        FrameBuffer.push_back(framing.getFrame().getElement(i));
-    }
-
-    //Afspiller lyd
-    Sound SoundOne;
-    SoundOne.playSound(FrameBuffer);
-
     return 0;
 }
